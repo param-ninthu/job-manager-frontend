65a6af42f7134b7b5a074d196812a4c4ac1de123
param-ninthu <ninthu1999@gmail.com>
2023-05-12 16:41:41 +0530

    finishing up the jobs screen

--- src/screens/JobsScreen.jsx
+++ src/screens/JobsScreen.jsx
@@ -4,6 +4,9 @@ import "./../assets/css/jobsScreen.css";
 
 import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
 import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import ErrorIcon from "@mui/icons-material/Error";
+import CancelIcon from "@mui/icons-material/Cancel";
 
 const JobsScreen = () => {
   const [jobs, setJobs] = useState([]);
@@ -20,7 +23,7 @@ const JobsScreen = () => {
         );
         setTotalPages(response.data.totalPages);
         setJobs(response.data.content);
-        console.log(response.data.content);
+        // console.log(response.data.content);
       } catch (error) {
         console.log(error);
       }
@@ -43,6 +46,44 @@ const JobsScreen = () => {
     setCurrentPage(currentPage + 1);
   };
 
+  const renderJobStatusIcon = (jobStatus) => {
+    switch (jobStatus) {
+      case "Completed":
+        return <CheckCircleIcon className="status-icon completed" />;
+      case "Failed":
+        return <ErrorIcon className="status-icon error" />;
+      case "Cancelled":
+        return <CancelIcon className="status-icon cancelled" />;
+      default:
+        return null;
+    }
+  };
+
+  const renderJobStatusColor = (jobStatus) => {
+    switch (jobStatus) {
+      case "Completed":
+        return (
+          <>
+            <div className="status-completed completed"> {jobStatus} </div>
+          </>
+        );
+      case "Failed":
+        return (
+          <>
+            <div className="status-error error"> {jobStatus} </div>
+          </>
+        );
+      case "Cancelled":
+        return (
+          <>
+            <div className="status-cancelled cancelled"> {jobStatus} </div>
+          </>
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="container">
       <h1 className="title">Jobs</h1>
@@ -50,35 +91,40 @@ const JobsScreen = () => {
         <table className="table">
           <thead className="table_head">
             <tr className="table_row">
-              <th>Job Status</th>
-              <th>Job Name</th>
+              <th className="icon"></th>
+
               <th>Job Owner</th>
+              <th>Job Name</th>
               <th>Job Status</th>
             </tr>
           </thead>
           <tbody className="table_body">
             {jobs.map((job) => (
               <tr className="table_row" key={job.id}>
-                <td>{job.jobStatus}</td>
-                <td>{job.name}</td>
+                <td className="icon"> {renderJobStatusIcon(job.jobStatus)}</td>
+
                 <td>{job.owner}</td>
-                <td>{job.jobStatus}</td>
+                <td>{job.name}</td>
+                <td>{renderJobStatusColor(job.jobStatus)}</td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
       <div className="footer">
-        <div onClick={handleNextPage}>
-          {" "}
-          <KeyboardDoubleArrowRightIcon />{" "}
-        </div>
-        <div className="subtitle">
-          {currentPage + 1} of {totalPages}
-        </div>
-        <div onClick={handlePrevPage}>
-          {" "}
-          <KeyboardDoubleArrowLeftIcon />{" "}
+        <div className="buttons">
+          <div onClick={handlePrevPage} className="arrow">
+            {" "}
+            <KeyboardDoubleArrowLeftIcon />{" "}
+          </div>
+
+          <div className="subtitle">
+            {currentPage + 1} of {totalPages}
+          </div>
+          <div onClick={handleNextPage} className="arrow">
+            {" "}
+            <KeyboardDoubleArrowRightIcon />{" "}
+          </div>
         </div>
       </div>
     </div>
