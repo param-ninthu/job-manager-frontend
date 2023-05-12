60fcc6a370626c2437093a1258b161a1d86d3860
param-ninthu <ninthu1999@gmail.com>
2023-05-12 23:46:25 +0530

    add user icons and css for expand job screen

--- src/screens/ExpandJobScreen.jsx
+++ src/screens/ExpandJobScreen.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import "./../assets/css/expandJobScreen.css";
 
 const ExpandJobScreen = () => {
-  return <div>ExpandJobScreen</div>;
+  return (
+    <div className="container">
+      <div className="primary_container error">
+        <div className="primary_container_header">
+          <div className="primary_container_header_left">
+            <h1>Job ID: 1</h1>
+            <div className="primary_container_header_left_status">
+              <div className="status-icon error"></div>
+              <div className="status-error error">Failed</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ExpandJobScreen;
